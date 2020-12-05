e2824cfc74bacfa2c2fee07b8af724027f3b6f36
Jack Franklin <jackhenryfranklin@gmail.com>
2020-12-05 17:38:14 +0000

    Improve day 4. Used regex for some functions. General code enhancements.

--- src/day04/day04.jl
+++ src/day04/day04.jl
@@ -1,125 +1,87 @@
 using AdventOfCode2020
 
-getInput() = readlines(getInputPath(4))
+getInput() = split(read(getInputPath(4), String), "\n\n")
 
-function part1()
-    requiredFields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-    input = getInput()
-    push!(input,"")
-    currentData = ""
-    validPasswords = 0
-    for row in input
-        if row == ""
-            # check
-            inData = [occursin(x,currentData) for x in requiredFields]
-            all(inData) && (validPasswords+=1)
-            currentData=""
-        else 
-            currentData *= ""
-            currentData *= row
-        end
-    end
-    return validPasswords
+function hasFields(passport::Dict{String,String})
+    requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    return all(haskey(passport, field) for field ∈ requiredFields)
 end
 
-function checkPassportIsValid(data)
-    fields = Dict{String,String}()
-    requiredFields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-    for f in requiredFields
-        locateField = findfirst(f*":",data)[end]
-        locateEndOfField = findnext(' ',data,locateField)
-        isnothing(locateEndOfField) && (locateEndOfField = length(data) + 1)
-        fields[f] = data[locateField+1:locateEndOfField-1]
+function hasFields(passport::AbstractString)
+    return hasFields(convertPassport(passport))
+end
+
+function part1()
+    input = getInput()
+    return sum(hasFields.(input))
+end
+
+function convertPassport(passport)
+    data = Dict{String,String}()
+    for match in eachmatch(r"(.{3}):([#\w]+)", passport)
+        data[match.captures[1]] = match.captures[2]
     end
-    # tests
-    passwordValid = true
-    for key in keys(fields)
-        if key == "byr"
-            yr = tryparse(Int,fields[key])
-            if isnothing(yr) || !(yr >= 1920 && yr <= 2002)
-                passwordValid = false
-                break
-            end
-        elseif key == "iyr"
-            yr = tryparse(Int,fields[key])
-            if isnothing(yr) || !(yr >= 2010 && yr <= 2020)
-                passwordValid = false
-                break
-            end
-        elseif key == "eyr"
-            yr = tryparse(Int,fields[key])
-            if isnothing(yr) || !(yr >= 2020 && yr <= 2030)
-                passwordValid = false
-                break
-            end
-        elseif key == "hgt"
-            system = fields[key][end-1:end]
-            height = tryparse(Int,fields[key][1:end-2])
-            if isnothing(height)
-                passwordValid = false
-                break
-            elseif system == "cm"
-                if !(height in 150:193)
-                    passwordValid = false
-                    break
-                end
-            elseif system == "in"
-                if !(height in 59:76)
-                    passwordValid = false
-                    break
-                end
-            else
-                passwordValid = false
-                break
-            end
-        elseif key == "hcl"
-            hcl = fields[key]
-            if !(hcl[1] == '#' && (length(hcl) == 7))
-                passwordValid = false
-                break
-            end
-            col = collect(hcl[2:end])
-            validCol = ['a' 'b' 'c' 'd' 'e' 'f' '0' '1' '2' '3' '4' '5' '6' '7' '8' '9']
-            validChar = [c in validCol for c in col]
-            if !all(validChar)
-                passwordValid = false
-                break
-            end
-        elseif key == "ecl"
-            validEyes = ["amb" "blu" "brn" "gry" "grn" "hzl"  "oth"]
-            if !(fields[key] in validEyes)
-                passwordValid = false
-                break
-            end
-        elseif key == "pid"
-            pid = fields[key]
-            numDigits = length(pid)
-            num = tryparse(Int,pid)
-            if numDigits != 9 || isnothing(num)
-                passwordValid = false
-                break
-            end
-        end
+    return data
+end
+
+function check_byr(byr)
+    year = tryparse(Int, byr)
+    return !(isnothing(year) || !(1920 <= year <= 2002))
+end
+
+function check_iyr(iyr)
+    year = tryparse(Int, iyr)
+    return !(isnothing(year) || !(2010 <= year <= 2020))
+end
+
+function check_eyr(eyr)
+    year = tryparse(Int, eyr)
+    return !(isnothing(year) || !(2020 <= year <= 2030))
+end
+
+function check_hgt(hgt)
+    height = tryparse(Int, hgt[1:end-2])
+    system = hgt[end-1:end]
+    isnothing(height) && return false
+    if system == "cm"
+        return (150 <= height <= 193)
+    elseif system == "in"
+        return (59 <= height <= 76)
     end
-    return passwordValid
+    return false
+end
+
+function check_hcl(hcl)
+    return (hcl[1] == '#') && occursin(r"(?<!.)[0-9a-f]{6}(?!.)",hcl[2:end])
+end
+
+function check_ecl(ecl)
+    return occursin(r"(?<!.)(amb|blu|brn|gry|grn|hzl|oth)(?!.)",ecl)
+end
+
+function check_pid(pid)
+    return occursin(r"(?<!.)([0-9]{9})(?!.)",pid)
+end
+
+function checkPassportIsValid(data::Dict{String,String})
+    return hasFields(data) &&
+        check_byr(data["byr"]) &&
+        check_iyr(data["iyr"]) &&
+        check_eyr(data["eyr"]) &&
+        check_hgt(data["hgt"]) &&
+        check_hcl(data["hcl"]) &&
+        check_ecl(data["ecl"]) &&
+        check_pid(data["pid"])
+end
+
+function checkPassportIsValid(passport::AbstractString)
+    return checkPassportIsValid(convertPassport(passport))
 end
 
 function part2()
-    requiredFields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
     input = getInput()
-    push!(input,"")
-    currentData = ""
-    validPasswords = 0
-    for row in input
-        if row == ""
-            # check
-            inData = [occursin(x*":",currentData) for x in requiredFields]
-            all(inData) && checkPassportIsValid(currentData) && (validPasswords+=1)
-            currentData=""
-        else 
-            currentData *= " "
-            currentData *= row
-        end
-    end
-    return validPasswords
+    return sum(checkPassportIsValid.(input))
 end
+
+println("part 1:", part1())
+println("part 2:", part2())
