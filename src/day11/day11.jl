4af4a7ea8d608c312fb5b81131b00b41c8ed0abe
Jack Franklin <jackhenryfranklin@gmail.com>
2020-12-11 11:14:36 +0000

    Improve efficiency of part 1

--- src/day11/day11.jl
+++ src/day11/day11.jl
@@ -5,11 +5,10 @@ function getinput()
     return hcat(inputArray...)
 end
 
-function getneighbours(cell, indices)
-    xcells = cell[1] .+ [-1 0 1]
-    ycells = cell[2] .+ [-1 0 1]
-    neighbourhood = [CartesianIndex(x, y) for x ∈ xcells, y ∈ ycells if CartesianIndex(x,y) != cell]
-    return intersect(neighbourhood, indices)
+function getneighbours(cell, arraySize)
+    xcells = filter(a->a ∈ 1:arraySize[1],cell[1] .+ [-1 0 1])
+    ycells = filter(a->a ∈ 1:arraySize[2],cell[2] .+ [-1 0 1])
+    return [CartesianIndex(x, y) for x ∈ xcells, y ∈ ycells if CartesianIndex(x,y) != cell]
 end
 
 function updatecell(cell, neighbours)
@@ -30,9 +29,10 @@ end
 
 function nextstep(array)
     newArray = copy(array)
+    arraySize = size(array)
     indices = CartesianIndices(array)
     for cell in eachindex(indices)
-        neighbourhood = getneighbours(cell, indices)
+        neighbourhood = getneighbours(cell, arraySize)
         current = array[cell]
         neighbours = array[neighbourhood]
         newArray[cell] = updatecell(current, neighbours)
@@ -46,7 +46,6 @@ function part1()
     currentStep = 0
     while !stopped
         currentStep += 1
-        println(currentStep)
         nextArray = nextstep(array)
         if nextArray == array
             stopped = true
