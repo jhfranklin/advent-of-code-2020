ef890a64a7501df8bd5d9150f22eb96629b085ed
Jack Franklin <jackhenryfranklin@gmail.com>
2020-12-17 09:52:29 +0000

    Update to allow a sample to be imported.

--- src/AdventOfCode2020.jl
+++ src/AdventOfCode2020.jl
@@ -2,8 +2,8 @@ module AdventOfCode2020
 
 export getInputPath
 
-function getInputPath(day::Int)
-    return joinpath(@__DIR__,"day" * lpad(day,2,"0"), "input.txt")
+function getInputPath(day::Int,input=true)
+    return joinpath(@__DIR__,"day" * lpad(day,2,"0"), input ? "input.txt" : "sample.txt")
 end
 
 end # module
